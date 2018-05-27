96caaf243a27b4efe1df9b77596da9463cf242a1
Julie K <julieskim160@gmail.com>
2018-05-26 17:42:07 -0700

    fix extra space

--- model/user.js
+++ model/user.js
@@ -24,6 +24,6 @@ return bcrypt.compare(password, this.password);
 
 userSchema.statics.hashPassword = function (password) {
     return bcrypt.hash(password, 10);
-  };
+};
   
 module.exports = mongoose.model('User', userSchema);
